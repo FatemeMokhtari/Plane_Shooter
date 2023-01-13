e01a092ddc1335543b05a760084581674626e91f
Callme Fateme <fatemeh4552@gmail.com>
2023-01-13 14:39:30 +0330

    Destroy Enemy And Add Explosion Effect

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
       public GameObject EnemyBullet;
+      public GameObject EnemyExplosionPrefab;
       public Transform GunPointer1;
       public Transform GunPointer2;
       public float EnemyBulletTime = 0.5f;
@@ -17,6 +18,15 @@ public class Enemy : MonoBehaviour
       {
             transform.Translate(UnityEngine.Vector2.down * speed * Time.deltaTime);
       }
+      private void OnTriggerEnter2D(Collider2D other)
+      {
+            if (other.tag == "PlayerBullet")
+            {
+                  Destroy(gameObject);
+                  GameObject EnemyExplosion = Instantiate(EnemyExplosionPrefab, transform.position, UnityEngine.Quaternion.identity);
+                  Destroy(EnemyExplosion,0.4f);
+            }
+      }
       void EnemyFire()
       {
             Instantiate(EnemyBullet, GunPointer1.position, UnityEngine.Quaternion.identity);
@@ -28,7 +38,7 @@ public class Enemy : MonoBehaviour
             while (true)
             {
                   yield return new WaitForSeconds(EnemyBulletTime);
-                  EnemyFire();                           
+                  EnemyFire();
             }
 
       }
